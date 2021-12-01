c4e7bea1f61253a32586056e5b0f728824fffdfd
Jeffae-Schroff <77805962+Jeffae-Schroff@users.noreply.github.com>
2021-12-01 12:37:19 -0800

    param_stats.py now makes histograms, also has 1000 tunes data

--- examples/dummy_data/data_scripts/param_stats.py
+++ examples/dummy_data/data_scripts/param_stats.py
@@ -2,16 +2,18 @@ import sys
 import os
 import numpy as np
 import statistics
+import matplotlib.pyplot as plt
 #prints stats of given file in many_tunes
 
 if len(sys.argv) != 2 :
     print('invalid arguments to gather_data.py')
 
-filename = sys.argv[1]
-if(not os.path.exists("many_tunes/"+filename)):
+filepath = sys.argv[1]
+if(not os.path.exists(filepath)):
     print('file given to oaram_stats.py does not exist')
 
-file_output = open("many_tunes/"+filename, 'r').readlines()
+filename = filepath.split('/')[-1]
+file_output = open(filepath, 'r').readlines()
 file_output = np.array([f.split() for f in file_output])
 
 params = file_output[0]
@@ -27,5 +29,15 @@ for i in range(len(params)):
     stdev = statistics.stdev(vals[i])
     print(params[i].ljust(20), str(mean).ljust(20), str(stdev).ljust(20))
 print()
+
+
+for i in range(len(params)):
+    
+    plt.figure()
+    plt.hist(vals[i], bins=30)
+    plt.title(filename)
+    plt.xlabel(params[i] + " value")
+    plt.ylabel("Frequency")
+    plt.savefig(filename+"_"+params[i]+".pdf")
 
 
--- examples/dummy_data/run_many.sh
+++ examples/dummy_data/run_many.sh
@@ -4,8 +4,11 @@ app-yoda2h5 MC --pname used_params -o inputdata.h5
 python3 data_scripts/add_err.py
 app-datadirtojson Data -o data.json
 
+start=$(date +%s)
+num_runs=1000
 python3 data_scripts/new_many_tunes.py
-for i in {1..30}; do 
+for ((i=1; i <= $num_runs; i++))
+do 
     app-build inputdata.h5  --order 3,0 --sample 30 -s $i -o val_30.json
     app-build inputdata.h5  --order 2,0 --sample 30 -s $i -o err_20.json --errs
     app-yodaenvelope val_30.json -o mytune/envelope
@@ -14,6 +17,8 @@ for i in {1..30}; do
     app-tune2 myweights.txt data.json val_30.json -e err_20.json -o tune_w_errs
     python3 data_scripts/gather_tune_data.py tune_no_errs
     python3 data_scripts/gather_tune_data.py tune_w_errs
+    current=$(date +%s)
+    printf "\n\n------------Run $i of $num_runs      Elapsed time: $(($current-$start)) s--------------\n\n"
 done
-python3 data_scripts/param_stats.py tune_no_errs
-python3 data_scripts/param_stats.py tune_w_errs
+python3 data_scripts/param_stats.py many_tunes/tune_no_errs
+python3 data_scripts/param_stats.py many_tunes/tune_w_errs
