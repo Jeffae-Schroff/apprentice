d76f14c37e2667dec3f06953816df6742ec24a2b
jeffae@berkeley.edu <jeffae@berkeley.com>
2023-04-12 02:58:23 -0700

    fix2

--- tuning_sampler/EvtGen/ZmumuPy8/run_all_cori.sh
+++ tuning_sampler/EvtGen/ZmumuPy8/run_all_cori.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+#SBATCH --image=hepstore/rivet-pythia
+#SBATCH --qos=shared
+#SBATCH --constraint=haswell
 
 for INDIR in 4alphaS_500runs_submit/*
 do
 	if [ -d $INDIR ];then
 		echo $INDIR
-		pythia8-main93 -c $INDIR/runPythia.cmnd -n 100000 -o $INDIR/combined
+		shifter pythia8-main93 -c $INDIR/runPythia.cmnd -n 100000 -o $INDIR/combined
 	fi
 done
